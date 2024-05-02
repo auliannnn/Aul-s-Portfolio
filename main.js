d1e5004925bda063170806939e7d8eb94df56496
Aulia Nisa <148309096+auliannnn@users.noreply.github.com>
2024-05-02 14:18:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var tombol = document.getElementById("button");
+console.log(tombol);
+
+function kirim (){
+    alert ('Your Mesagge Send!');
+}
+
+var judul = document.getElementById('cover');
+console.log('judul');
